cba7f19352402a06b85f00bce71f85c08e425ab3
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-11-21 12:46:18 +0100

    implement potential solution for collision

--- src/hash_table/hash_table.lua
+++ src/hash_table/hash_table.lua
@@ -2,17 +2,33 @@ local utils = require('hash_table.utils')
 
 function HashTable()
   local size = 1000
-  local buckets = utils.table_init(size, nil)
+  local buckets = utils.table_init(size, {})
 
   return {
     set = function(key, value)
-      local hash_key = utils.hashify(key)
-      buckets[hash_key] = value
+      local hash_key = utils.hashify(key, size)
+      local item = buckets[hash_key]
+      local found = utils.table_find(function(item)
+        return item[0] == key
+      end)
+
+      if found then
+        found[1] = value
+      else
+        table.insert(item, { key, value })
+      end
     end,
 
     get = function(key)
-      local hash_key = utils.hashify(key)
-      return buckets[hash_key]
+      local hash_key = utils.hashify(key, size)
+      local item = buckets[hash_key]
+      local found = utils.table_find(function(item)
+        return item[0] == key
+      end)
+
+      if found then
+        return found[1]
+      end
     end,
   }
 end
--- src/hash_table/utils.lua
+++ src/hash_table/utils.lua
@@ -8,17 +8,28 @@ function table_init(size, value)
   return output
 end
 
-function hashify(key)
+function table_find(array, callback)
+  for i=1, #array do
+    local item = array[i]
+
+    if callback(item) then
+      return item
+    end
+  end
+end
+
+function hashify(key, size)
   local hash_key = 0
 
   for i=1, #key do
     hash_key = hash_key + string.byte(key, i)
   end
 
-  return hash_key
+  return (hash_key % size)
 end
 
 return {
   table_init = table_init,
+  table_find = table_find,
   hashify = hashify
 }
