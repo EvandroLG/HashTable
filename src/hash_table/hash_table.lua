f82f79582f51b3b32fa3629a3e540ad2765ba819
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-11-21 13:55:59 +0100

    fix issues related to the collision logic

--- src/hash_table/hash_table.lua
+++ src/hash_table/hash_table.lua
@@ -8,12 +8,12 @@ function HashTable()
     set = function(key, value)
       local hash_key = utils.hashify(key, size)
       local item = buckets[hash_key]
-      local found = utils.table_find(function(item)
-        return item[0] == key
+      local found = utils.table_find(item, function(element)
+        return element[1] == key
       end)
 
       if found then
-        found[1] = value
+        found[2] = value
       else
         table.insert(item, { key, value })
       end
@@ -22,12 +22,12 @@ function HashTable()
     get = function(key)
       local hash_key = utils.hashify(key, size)
       local item = buckets[hash_key]
-      local found = utils.table_find(function(item)
-        return item[0] == key
+      local found = utils.table_find(item, function(element)
+        return element[1] == key
       end)
 
       if found then
-        return found[1]
+        return found[2]
       end
     end,
   }
--- test.lua
+++ test.lua
@@ -3,6 +3,12 @@ local HashTable = require 'hash_table'
 
 test('hash', function(a)
   local table = HashTable()
+  table.set('name', 'Evandr')
+  table.set('age', 33)
+
+  a.equal(table.get('name'), 'Evandr')
+  a.equal(table.get('age'), 33)
+
   table.set('name', 'Evandro')
   table.set('age', 34)
 
